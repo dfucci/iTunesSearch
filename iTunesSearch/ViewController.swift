5e8f83e1302b08393ad3beabb26bc2463a6727be
dfucci <davfuc@gmail.com>
2014-10-19 19:24:27 +0300

    Initial Commit

---
+++ iTunesSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Davide Fucci on 19/10/14.
+//  Copyright (c) 2014 Davide Fucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
